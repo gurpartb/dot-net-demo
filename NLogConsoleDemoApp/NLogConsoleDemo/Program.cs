bb204aeba7a2fdf85dd8d9aca9f4c3039be7c103
Gurpartap BHATTI <gurpartap.bhatti@amadeus.com>
2022-05-01 18:02:10 -0600

    nlog console app demo

---
+++ NLogConsoleDemoApp/NLogConsoleDemo/Program.cs
@@ -0,0 +1,14 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+var logger = NLog.LogManager.GetCurrentClassLogger();
+logger.Info("App Started {args} {username}",args,"fullstackamigo");
+try
+{
+
+}
+catch (Exception e)
+{
+
+    logger.Error(e, "This was unexpected! {args}", 2);
+}
+NLog.LogManager.Shutdown();
